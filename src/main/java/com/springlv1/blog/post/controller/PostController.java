39d06285522655227384ad9a2116a3d934cc2568
rnjswo9578 <rnjswo153486@gmail.com>
2023-04-17 09:35:26 +0900

    rename getPostlist name

--- src/main/java/com/springlv1/blog/post/controller/PostController.java
+++ src/main/java/com/springlv1/blog/post/controller/PostController.java
@@ -30,7 +30,7 @@ public class PostController {
     }
 
 
-    @GetMapping("/list")
+    @GetMapping("/posts")
     public List<PostResponseDto> getPostList(){
         return postService.getPostList();
     }
